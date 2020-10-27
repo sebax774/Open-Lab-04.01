6e12b408ec469a06cdbc92a4440cdf2ba6d0821e
sebax774 <bugansebastian@gmail.com>
2020-10-27 14:04:03 +0100

    vypracovanie

--- Open-Lab-04.01/Checker.cs
+++ Open-Lab-04.01/Checker.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_04._01
     {
         public bool DoubleLetters(string str)
         {
-            throw new NotImplementedException();
+            int lenght = str.Length;
+            for (int i = 0; i < lenght - 1; i++)
+            {
+                if (str[i] == str[i + 1])
+                    return true;
+            }
+            return false;
         }
     }
 }
